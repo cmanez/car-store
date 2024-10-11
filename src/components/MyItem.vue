b99aa2d0325f4096b9fa6a690f5ab78a521f0ff0
Anton <cecon_76@mail.ru>
2024-10-11 13:33:01 +0500

    add car item component demo

---
+++ src/components/MyItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="item">
+    <div class="item_car-pic"><img src="@/assets/img/car.webp" alt="" /></div>
+    <div class="item_car-model">Модель:</div>
+    <div class="item_car-price">Цена:</div>
+  </div>
+</template>
+
+<script setup></script>
+<style lang="scss">
+.item {
+  display: flex;
+  padding: 0.5rem;
+  flex-direction: column;
+  justify-content: flex-end;
+  flex-grow: 1;
+  border: solid 1px rgb(184, 127, 127);
+  border-radius: 10px;
+  background-color: $main-light;
+  &_car-pic {
+    height: 60%;
+    align-self: center;
+    justify-self: center;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  &_car-model {
+    height: 20%;
+  }
+  &_car-price {
+    height: 20%;
+  }
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="main-layout">
     <MyNav></MyNav>
-    <div style="margin: 0 auto">ХУЙ</div>
+    <div class="item-grid">
+      <MyItem></MyItem>
+      <MyItem></MyItem>
+      <MyItem></MyItem>
+      <MyItem></MyItem>
+      <MyItem></MyItem>
+      <MyItem></MyItem>
+    </div>
     <MyFooter></MyFooter>
   </div>
 </template>
@@ -9,6 +16,7 @@
 <script setup>
 import MyNav from '@/components/MyNav.vue'
 import MyFooter from '@/components/MyFooter.vue'
+import MyItem from '@/components/MyItem.vue'
 </script>
 
 <style lang="scss">
@@ -17,5 +25,13 @@ import MyFooter from '@/components/MyFooter.vue'
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  .item-grid {
+    display: grid;
+    height: 100%;
+    padding: 2rem;
+    grid-template-columns: repeat(auto-fill, 19%);
+    grid-template-rows: repeat(auto-fill, 30%);
+    gap: 10px;
+  }
 }
 </style>
