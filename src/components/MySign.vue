5cb2f710670c9bfbfa5d2d55fabbfd59aea3e1f1
Anton <cecon_76@mail.ru>
2024-10-21 18:57:26 +0500

    add sign in logic validation form

--- src/components/MySign.vue
+++ src/components/MySign.vue
@@ -4,24 +4,53 @@
       <span>Регистрация</span>
       <div class="sign-form_item">
         <span class="sign-form_item_text"> Логин:</span>
-        <div class="sign-form_item_input"><MyInput v-model="login" id=""></MyInput></div>
+        <div class="sign-form_item_input"><MyInput v-model="signState.login" id=""></MyInput></div>
       </div>
 
       <div class="sign-form_item">
         <span class="sign-form_item_text">Пароль:</span>
-        <div class="sign-form_item_input"><MyInput v-model="password" id=""></MyInput></div>
+        <div class="sign-form_item_input">
+          <MyInput
+            v-model="signState.password"
+            @click="console.log(signState.checkConfirmPassword)"
+            id=""
+            :class="{
+              validpass: signState.checkConfirmPassword === true,
+              invalidpass: signState.checkConfirmPassword === false
+            }"
+          ></MyInput>
+        </div>
       </div>
 
       <div class="sign-form_item">
         <span class="sign-form_item_text">Повторите пароль:</span>
-        <div class="sign-form_item_input"><MyInput id=""></MyInput></div>
+        <div class="sign-form_item_input">
+          <!-- {{ signState. }} -->
+          <MyInput
+            id=""
+            :class="{
+              validpass: signState.checkConfirmPassword === true,
+              invalidpass: signState.checkConfirmPassword === false
+            }"
+            v-model="signState.confirmPassword"
+          ></MyInput>
+        </div>
       </div>
 
       <div class="sign-form_item">
         <span class="sign-form_item_text">Электронная почта:</span>
-        <div class="sign-form_item_input"><MyInput v-model="email" id=""></MyInput></div>
+        <div class="sign-form_item_input">
+          <MyInput
+            id=""
+            :class="{
+              validpass: signState.checkValideEmail === true,
+              invalidpass: signState.checkValideEmail === false
+            }"
+            v-model="signState.email"
+          ></MyInput>
+        </div>
       </div>
-      <div><MyButton>Зарегистрироваться</MyButton></div>
+      <div><MyButton @click="signState.startCheckValid">Зарегистрироваться</MyButton></div>
     </div>
   </MyModal>
 </template>
@@ -29,14 +58,22 @@
 import MyModal from './MyModal.vue'
 import MyInput from '@/UI/MyInput.vue'
 import MyButton from '@/UI/MyButton.vue'
-import { ref } from 'vue'
+// import { ref } from 'vue'
+import { signStore } from '@/stores/signin'
 
-const login = ref('')
-const password = ref('')
-const email = ref('')
+// const password = ref('')
+// const confrimPassword = ref('')
+
+const signState = signStore()
 </script>
 
 <style lang="scss">
+.validpass {
+  background-color: rgba($color: green, $alpha: 0.5);
+}
+.invalidpass {
+  background-color: rgba($color: red, $alpha: 0.5);
+}
 .sign_none {
   display: none;
 }
--- src/stores/signin.js
+++ src/stores/signin.js
@@ -1,3 +1,46 @@
 import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
 
-export const signStore = defineStore('sign', () => {})
+export const signStore = defineStore('sign', () => {
+  const login = ref('')
+  const email = ref('')
+  const password = ref('')
+  const confirmPassword = ref('')
+  const classPasswordChange = ref(null)
+  const clickToCheck = ref(false)
+
+  const startCheckValid = () => {
+    clickToCheck.value = true
+  }
+
+  const checkConfirmPassword = computed(() => {
+    if (clickToCheck.value && password.value && confirmPassword.value) {
+      password.value === confirmPassword.value
+        ? (classPasswordChange.value = true)
+        : (classPasswordChange.value = false)
+    }
+    return classPasswordChange.value
+  })
+
+  const checkValideEmail = computed(() => {
+    return clickToCheck.value ? /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i.test(email.value) : null
+  })
+  const checkStrongPassword = computed(() => {
+    return clickToCheck.value
+      ? /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/.test(
+          password.value
+        )
+      : null
+  })
+
+  return {
+    checkConfirmPassword,
+    startCheckValid,
+    password,
+    confirmPassword,
+    login,
+    email,
+    checkValideEmail,
+    checkStrongPassword
+  }
+})
